82aae1a4a1b8287631ae3946dec12c2b8d9620eb
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-12-18 21:59:51 +0000

    chore(py): Simple python script to launch a chromium w/ linux container with docker

---
+++ a.py
@@ -0,0 +1,25 @@
+import subprocess
+
+# 1. Force remove the old container if it exists
+subprocess.run(["docker", "rm", "-f", "chromium"], capture_output=True)
+
+# 2. Run the new container
+docker_cmd = [
+    "docker", "run", "-d",
+    "--name=chromium",
+    "-e", "PUID=1000",
+    "-e", "PGID=1000",
+    "-e", "TZ=Etc/UTC",
+    "-p", "3000:3000",
+    "-p", "3001:3001",
+    "-v", "/path/to/chromium/config:/config",
+    "--shm-size=1gb",
+    "--restart", "unless-stopped",
+    "lscr.io/linuxserver/chromium:latest"
+]
+subprocess.run(docker_cmd)
+
+x = True
+
+while (x):
+    print("motivation is key!")
