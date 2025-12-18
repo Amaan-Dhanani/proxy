84b2b653df1de4a6851583ef5cda35bbe66a3bc0
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-12-18 22:15:17 +0000

    chore(py): Enabled sidebar - now there is sharing, audio, video, and other settings.

--- a.py
+++ a.py
@@ -10,6 +10,7 @@ docker_cmd = [
     "-e", "PUID=1000",
     "-e", "PGID=1000",
     "-e", "TZ=Etc/UTC",
+    "-e", "SELKIES_UI_SHOW_SIDEBAR=true",
     "-p", "3000:3000",
     "-p", "3001:3001",
     "-v", "/path/to/chromium/config:/config",
